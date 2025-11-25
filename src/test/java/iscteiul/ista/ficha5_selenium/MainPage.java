bdf9fc2bd3df748e00b2a902d3199e0c1761cf34
LETI_122607 <gctso@iscte-iul.pt>
2025-11-25 15:24:43 +0000

    tirar o dolar, agr temos o pom do selenium mas sem os dolares

--- src/test/java/iscteiul/ista/ficha5_selenium/MainPage.java
+++ src/test/java/iscteiul/ista/ficha5_selenium/MainPage.java
@@ -1,13 +1,25 @@
 package iscteiul.ista.ficha5_selenium;
-import com.codeborne.selenide.SelenideElement;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$x;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 // page_url = https://www.jetbrains.com/
 public class MainPage {
-  public SelenideElement seeDeveloperToolsButton = $x("//*[@data-test-marker='Developer Tools']");
-  public SelenideElement findYourToolsButton = $x("//*[@data-test='suggestion-action']");
-  public SelenideElement toolsMenu = $x("//div[@data-test='main-menu-item' and @data-test-marker = 'Developer Tools']");
-  public SelenideElement searchButton = $("[data-test='site-header-search-action']");
+    @FindBy(xpath = "//*[@data-test-marker='Developer Tools']")
+    public WebElement seeDeveloperToolsButton;
+
+    @FindBy(xpath = "//*[@data-test='suggestion-action']")
+    public WebElement findYourToolsButton;
+
+    @FindBy(xpath = "//div[@data-test='main-menu-item' and @data-test-marker = 'Developer Tools']")
+    public WebElement toolsMenu;
+
+    @FindBy(css = "[data-test='site-header-search-action']")
+    public WebElement searchButton;
+
+    public MainPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
 }
