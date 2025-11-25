e3dda3f355f02d22dd7335a302d1b9ae37a07158
LETI_122607 <gctso@iscte-iul.pt>
2025-11-25 16:56:56 +0000

    parte A feita

--- src/test/java/iscteiul/ista/ficha5_selenium/MainPage.java
+++ src/test/java/iscteiul/ista/ficha5_selenium/MainPage.java
@@ -1,5 +1,4 @@
 package iscteiul.ista.ficha5_selenium;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -10,7 +9,7 @@ public class MainPage {
     @FindBy(xpath = "//*[@data-test-marker='Developer Tools']")
     public WebElement seeDeveloperToolsButton;
 
-    @FindBy(xpath = "//*[@data-test='suggestion-action']")
+    @FindBy(xpath = "//*[@data-test='suggestion-link']")
     public WebElement findYourToolsButton;
 
     @FindBy(xpath = "//div[@data-test='main-menu-item' and @data-test-marker = 'Developer Tools']")
@@ -19,7 +18,9 @@ public class MainPage {
     @FindBy(css = "[data-test='site-header-search-action']")
     public WebElement searchButton;
 
+    @FindBy(css = ".ch2-allow-all-btn")
+    public WebElement acceptCookiesButton;
+
     public MainPage(WebDriver driver) {
-        PageFactory.initElements(driver, this);
-    }
+        PageFactory.initElements(driver,this);}
 }
